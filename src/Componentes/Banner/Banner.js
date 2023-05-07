8abfa26afb53d06ccacb7a17b83b1a2f0147351c
Clairtonlm <clairtonlm@gmail.com>
2023-05-06 22:21:22 -0300

    Criando Banner da página

---
+++ src/Componentes/Banner/Banner.js
@@ -0,0 +1,16 @@
+//
+import './Banner.css'
+const Banner = ()=>{
+    //JSX--> como js trabalha a parte visual
+    return(
+        <>
+        <header className="banner">
+             <img src="/imagens/banner.png" alt="banner da página"/>
+        </header>
+       
+        
+        
+        </>
+    )
+}
+export default Banner;
