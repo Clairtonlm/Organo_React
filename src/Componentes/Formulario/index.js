27ef75035f2b89d289bf315f6017e905e12c5333
Clairtonlm <clairtonlm@gmail.com>
2023-05-07 22:47:23 -0300

    Evento onSubimit no form

--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -16,16 +16,20 @@ const Formulario = ()=>{
         'Ux e Design',
         'Gestão'
     ]
+    const aoSalvar = (evento)=>{
+        evento.preventDefault();
+        console.log("Formulário foi enviado com sucesso")
+    }
 
     return(
         <section className="formulario">
-            <form action="">
+            <form onSubmit ={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-            <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-            <CampoTexto label="Cargo" placeholder="Digite seu Cargo"/>
-            <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
-            <ListaSuspensa label="Time" itens={times}/>
-            <Botao texto="Criar Card"/>
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome"/>
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu Cargo"/>
+                <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao texto="Criar Card"/>
             
             </form>
         </section>
