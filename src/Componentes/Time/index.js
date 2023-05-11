915a5829a2e02963c1356e2f6d2cac0fc88cbbb4
Clairtonlm <clairtonlm@gmail.com>
2023-05-11 19:49:47 -0300

    Renderizando Codicional no react

--- src/Componentes/Time/index.js
+++ src/Componentes/Time/index.js
@@ -6,7 +6,8 @@ import'./Time.css'
 const Time = (props)=>{
     const css = { backgroundColor: props.corSecundaria}
     return (
-        <section className='time' style={css}>
+        
+        props.colaboradores.length > 0 && <section className='time' style={css}>
             <h3 style={{ borderColor: props.corprimaria}}>{props.nome}</h3>
             <div className='colaboradores'>
             {props.colaboradores.map(colaborador => 
@@ -18,6 +19,7 @@ const Time = (props)=>{
            
           
         </section>
+       
     )
 }
 export default Time;
